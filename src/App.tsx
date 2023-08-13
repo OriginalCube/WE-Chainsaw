b15deb254bbf6043122307305d11f4a74be0e592
lancemiraflorcube@gmail.com <lancemiraflorcube@gmail.com>
2023-08-13 22:35:26 +0800

    Adjustsed New Songs

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import Player from "./components/Player";
 function App() {
   return (
     <div className="bg-slate-900">
-      <div className="absolute w-screen h-screen object-fill">
+      <div className="absolute w-screen h-screen object-fill bg-slate-900">
         {/* Bg filter color */}
       </div>
-      <div className="absolute h-screen w-screen flex items-center justify-center">
-        <video
-          src="./assets/videos/input.mp4"
-          className="bg-white absolute h-full w-auto"
-        ></video>
-      </div>
+      <div className="absolute h-screen w-screen flex items-center justify-center"></div>
       <Player />
     </div>
   );
